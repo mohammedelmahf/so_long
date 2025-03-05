13ec45f9b1b9dc184238807c090db296697737e8
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-03-05 20:43:41 +0000

    125%

--- sources/so_long_utils/so_long_utils_01.c
+++ sources/so_long_utils/so_long_utils_01.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 10:55:25 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/03/05 20:38:53 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/03/05 20:43:30 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,22 +48,26 @@ void	destroy_player_ass(t_build *build)
 	mlx_destroy_image(build->mlx, build->player_ass_u[3]);
 	mlx_destroy_image(build->mlx, build->player_ass_u[4]);
 	mlx_destroy_image(build->mlx, build->player_ass_u[5]);
+	mlx_destroy_image(build->mlx, build->player_ass_u[6]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[0]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[1]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[2]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[3]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[4]);
 	mlx_destroy_image(build->mlx, build->player_ass_l[5]);
+	mlx_destroy_image(build->mlx, build->player_ass_l[6]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[0]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[1]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[2]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[3]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[4]);
 	mlx_destroy_image(build->mlx, build->player_ass_r[5]);
+	mlx_destroy_image(build->mlx, build->player_ass_r[6]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[0]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[1]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[2]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[3]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[4]);
 	mlx_destroy_image(build->mlx, build->player_ass_d[5]);
+	mlx_destroy_image(build->mlx, build->player_ass_d[6]);
 }

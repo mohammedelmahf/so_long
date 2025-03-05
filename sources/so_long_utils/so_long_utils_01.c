4e1e94530ecd70c9fc06c9767bc4a7f6d32bf6d8
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-03-05 20:39:14 +0000

    125%

--- sources/so_long_utils/so_long_utils_01.c
+++ sources/so_long_utils/so_long_utils_01.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 10:55:25 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/03/01 12:25:23 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/03/05 20:38:53 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	destroy_game(t_build *build)
 		if (build->win)
 		{
 			mlx_destroy_window(build->mlx, build->win);
-			// mlx_destroy_image(build->mlx, build->exit_ass);
+			mlx_destroy_image(build->mlx, build->exit_ass[0]);
 			mlx_destroy_image(build->mlx, build->map_ass[0]);
 			mlx_destroy_image(build->mlx, build->map_ass[1]);
 			mlx_destroy_image(build->mlx, build->c_ass[0]);
@@ -27,8 +27,9 @@ int	destroy_game(t_build *build)
 			mlx_destroy_image(build->mlx, build->c_ass[2]);
 			mlx_destroy_image(build->mlx, build->c_ass[3]);
 			mlx_destroy_image(build->mlx, build->c_ass[4]);
+			mlx_destroy_image(build->mlx, build->c_ass[5]);
+			mlx_destroy_image(build->mlx, build->enemey_ass_d[0]);
 			destroy_player_ass(build);
-			destroy_enemy_ass(build);
 		}
 		mlx_destroy_display(build->mlx);
 		free(build->mlx);
@@ -36,18 +37,6 @@ int	destroy_game(t_build *build)
 	free_array(build->map);
 	exit(0);
 	return (0);
-}
-
-void	destroy_enemy_ass(t_build *build)
-{
-	// mlx_destroy_image(build->mlx, build->enemey_ass_u[0]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_u[1]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_l[0]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_l[1]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_r[0]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_r[1]);
-	mlx_destroy_image(build->mlx, build->enemey_ass_d[0]);
-	// mlx_destroy_image(build->mlx, build->enemey_ass_d[1]);
 }
 
 void	destroy_player_ass(t_build *build)
